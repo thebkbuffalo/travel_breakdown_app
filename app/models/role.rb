b0e250f0539463023a73316c83eb279386c48773
apgregory <aaron.p.gregory@gmail.com>
2014-10-02 16:23:45 -0400

    adds models and models tests

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,6 @@
 class Role < ActiveRecord::Base
+  validates :permission, :event_id, :user_id, presence: true
+  has_one :user
+  has_one :event
+  has_many :costs, :through => :event
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :admin, :pasword_digest, presence: true
+  validates :email, presence: true, uniqueness: true
+  has_many :roles
+  has_many :events, through: :role
 end
