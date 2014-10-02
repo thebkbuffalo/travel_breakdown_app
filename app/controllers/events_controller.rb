d88692e674e2a079ee73fd7d9da68b09ee952087
ericak11 <kantor.erica@gmail.com>
2014-10-02 17:15:56 -0400

    fixed user and events paths

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
+    @user = User.find(params[:user_id])
     @event = Event.new
   end
 
@@ -25,7 +26,6 @@ class EventsController < ApplicationController
   # POST /events.json
   def create
     @event = Event.new(event_params)
-
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
