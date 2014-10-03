f2d56c05641bd237651915f1d685b93071305883
apgregory <aaron.p.gregory@gmail.com>
2014-10-02 20:33:18 -0400

    adds annotation for schema to models

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ end
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'annotate'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id          :integer          not null, primary key
+#  start_date  :date
+#  end_date    :date
+#  amount_owed :decimal(, )
+#  permission  :string(255)
+#  paid        :boolean          default(FALSE)
+#  event_id    :integer
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Role < ActiveRecord::Base
   validates :permission, :event_id, :user_id, presence: true
   validates :paid, :inclusion => {:in => [true, false]}
