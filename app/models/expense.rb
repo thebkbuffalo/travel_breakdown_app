51a34908fdf14dbdd8c37344ca49cc3a8fbcde6f
graysonthemason <gray.reinhard@gmail.com>
2014-10-03 15:43:57 -0400

    all three expense models workinggit add .

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,8 +4,46 @@ class Expense < ActiveRecord::Base
   validates :event_id, :user_id, presence: true
   validates :approved, :inclusion => {:in => [true, false]}
   belongs_to :user
+  	def expense_event
+  		Event.find_by(id: event_id)
+  	end
+	def total_days
+		(end_date - start_date).to_i
+	end
+	 def expense_role
+    	 	Role.where(user_id: user_id, event_id: expense_event.id)[0]
+    	 end
+    def groceries
+    	count = 0
+    	sum = 0
+    	while count < expense_event.total_days
+    		if expense_event.start_date  + count == start_date && end_date >= expense_event.start_date + count
+    			sum += expense_event.attendance[count].count
+    			self.start_date += 1
+    			count += 1
+    		else
+    			count += 1
+    		end
+    	 end 
+    	 amount.to_f/sum * (expense_role.total_days)
+	end
+	def boat
+		per_day = amount.to_f/total_days
+		count = 0
+		sum = 0
+		while count < expense_event.total_days
+			if expense_event.start_date + count == start_date && end_date >= expense_event.start_date + count
+				sum += per_day/expense_event.attendance[count].count if expense_event.start_date + count >= expense_role.start_date && expense_event.start_date + count <= expense_role.end_date
+				self.start_date += 1
+				count += 1
+			else 
+				count += 1
 
-    def flatperday
+			end
+		end
+	end
+	def gift
+		amount.to_f / expense_event.event_users.count
 	end
 end
 
