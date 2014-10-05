17e1344941a69afd63f52f9aa5a44ed07379ec72
graysonthemason <gray.reinhard@gmail.com>
2014-10-05 13:46:26 -0400

    migration changes role table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004175108) do
+ActiveRecord::Schema.define(version: 20141005171211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,9 @@ ActiveRecord::Schema.define(version: 20141004175108) do
     t.string   "calculation_type"
     t.string   "tag"
     t.boolean  "approved",         default: false
-    t.integer  "event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "role_id"
   end
 
   create_table "roles", force: true do |t|
