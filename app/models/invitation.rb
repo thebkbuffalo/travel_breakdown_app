98140ad78499d3631c46986226aac02233b88f7d
apgregory <aaron.p.gregory@gmail.com>
2014-10-06 18:46:27 -0400

    saves events table option to show grayed out pending and invited

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,7 +1,8 @@
 class Invitation < ActiveRecord::Base
   belongs_to :event
-  validates :email, :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
-  validates :message, :length => { :minimum => 10, :maximum => 1000 }
+  validates :email, format: { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+  validates :message, length: { :minimum => 10, :maximum => 1000 }
+  validates :event_id, presence: true
 
   # validates :name,
   #           :presence => true
